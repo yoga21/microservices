10b6c8526ec41f66e730d1a126a06c3125aa75bc
yoga21 <50547906+yoga21@users.noreply.github.com>
2020-04-04 16:49:21 +0530

    Add files via upload

---
+++ Item-Catelog/src/main/java/com/example/demo/ItemCatelogApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemCatelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemCatelogApplication.class, args);
+	}
+
+}
